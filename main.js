76f0b927252f6132cf925763b461e0c9ac57ae3a
Asya Kamaeva <as.kamaeva@yandex.ru>
2020-03-26 15:59:14 +0300

    аккордеон, слайдер

--- main.js
+++ main.js
@@ -2,32 +2,68 @@ const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
 const name = document.querySelectorAll(".team__name");
 const menu = document.querySelectorAll(".menu__item");
-// let x;
+const left = document.querySelector(".slider__control_prev");
+const right = document.querySelector(".slider__control_next");
+const list = document.querySelector(".slider__list");
+
+
 
 hamburger.addEventListener('click', function(){
     nav.classList.toggle("nav_active");
     hamburger.classList.toggle("hamburger_active");
 });
 
+
 for (let i = 0; i<name.length; i++){
-    
     name[i].addEventListener('click', function(){
-        // x = document.querySelectorAll(".team__name_active");
 
-        name[i].classList.toggle("team__name_active");
-        
+        if (name[i].classList.value == "team__name team__name_active"){
+        name[i].classList.toggle('team__name_active');
+        } else {
+            if(document.querySelector(".team__name_active")){
+            document.querySelector(".team__name_active").classList.remove('team__name_active');
+            }
+
+            name[i].classList.toggle("team__name_active");
+        }    
     });
-
 }
 
-for (let i = 0; i<name.length; i++){
-    
+
+
+
+for (let i = 0; i<menu.length; i++){
     menu[i].addEventListener('click', function(){
         event.preventDefault();
-        menu[i].classList.toggle("menu__item_active");
+
+if (menu[i].classList.value == "menu__item menu__item_active"){
+    menu[i].classList.remove('menu__item_active');
+} else{
+    if(document.querySelector(".menu__item_active")){
+        document.querySelector(".menu__item_active").classList.remove('menu__item_active');
+    }
+    menu[i].classList.toggle("menu__item_active");
+}
         
     });
 
 }
 
+left.addEventListener("click", function (e){
+    loop ("left", e);
+});
+
+right.addEventListener("click", function (e){
+    loop ("right", e);
+});
+
+function loop(direction, e){
+    e.preventDefault();
+
+    if (direction === "right"){
+        list.appendChild(list.firstElementChild);
+    } else{
+        list.insertBefore(list.lastElementChild, list.firstElementChild);
+    }
+}
 
